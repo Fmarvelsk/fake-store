5f30e4939cea89eaf595b54a1b451910859cebcd
fmarvelsk <fmarvelsk@gmail.com>
2020-11-26 18:45:39 +0100

    eslint-ignore

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,7 +1,7 @@
 import { useStateValue } from "../StateProvider";
 import React from 'react';
 import Product from './Product';
-import { Row Container } from 'react-bootstrap'
+import { Row, Container } from 'react-bootstrap'
 const ProductItem = () => {
     // eslint-disable-next-line
 const [{product}, dispatch] = useStateValue()
